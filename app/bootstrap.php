35f2015336f07d40053475b38d69813149dc5bfb
imwaa <walid.dev@outlook.fr>
2021-01-11 23:33:08 +0100

    configured libraries with "sql_autoload_register"

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,5 +1,9 @@
 <?php
-    // LOAD LIBRARIES
-    require_once 'libraries/Controller.php';
-    require_once 'libraries/Core.php';
-    require_once 'libraries/Database.php';
+// LOAD CONFIG
+require_once 'config/config.php';
+
+// AUTOLOAD CORE LIBRARIES
+// IL REQUIRE LES LIBRARIES NECESSAIRES AUTOMATIQUEMENT
+spl_autoload_register(function ($className) {
+    require_once 'libraries/' . $className . '.php';
+});
---
+++ app/config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+// APP ROOT
+define ('APPROOT',  dirname(dirname(__FILE__)));
+// URL ROOT
+define("URLROOT", 'http://localhost/sharepostMVC/');
+// SITE NAME
+define("SITENAME", 'sharedpost');
